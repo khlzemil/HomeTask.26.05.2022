c3807f4c22ac39e7b577a4c28fa5e028886adec6
khlzemil <emilmg@code.edu.az>
2022-05-26 18:58:08 +0400

    HomeTask

---
+++ HomeTask.26.05-main/HomeTask.26.05/HomeTask.26.05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeTask._26._05
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Student student1 = new Student();
+            student1.Grades = new[] { 1, 2, 3, 4, 5, 6 };
+
+
+            foreach (var item in student1.Grades)
+            {
+                Console.WriteLine(item);
+            }       
+        }      
+    }   
+}
---
+++ HomeTask.26.05-main/HomeTask.26.05/HomeTask.26.05/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask._26._05
+{
+    public class Student
+    {
+        private int[] _newGrades;
+        public int[] Grades
+        {
+            get
+            {
+                return _newGrades;
+            }
+            set
+            {
+                int[] evenArr = new int[0];
+                int i = 0;
+                for(int j = 0; j < value.Length; j++)
+                {
+                    if (value[j] % 2 == 0)
+                    {
+                        Array.Resize(ref evenArr, evenArr.Length+1);
+                        evenArr[i] = value[j];
+                        i++;
+                    }
+
+                }
+                if(evenArr.Length > 0)
+                {
+                    _newGrades = evenArr;
+                }
+                else
+                {
+                    _newGrades = new int[] { 0,0,0,0,0,0 };
+                }
+
+
+            }
+        }
+
+    }
+}
+        
+
+    
+
